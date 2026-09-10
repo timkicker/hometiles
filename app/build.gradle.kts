ab4951bfa4d5fbc774cdcba34fa7cba5d52af70b
Tim Jochen Kicker <tim@kicker.dev>
2026-09-10 11:11:35 +0300

    version 0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "dev.kicker.hometiles"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
         resourceConfigurations += listOf("en", "de", "fr", "es", "it")
     }
 
